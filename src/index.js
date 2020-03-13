17a1c75f62ec3c6d87c6530f561a6729e994b4e0
eleex <eleex1988@gmail.com>
2020-03-13 17:06:12 +0200

    useContext

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MyContext = React.createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <MyContext.Provider value="Privet Mirrrr!">
+            <Hello />
+        </MyContext.Provider>
+    );
+};
+
+const Hello = () => {
+    const value = useContext(MyContext);
+    return <p>{value}</p>
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
